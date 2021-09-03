841c5c08c700185c25a300a3250a62b787c445fe
Avaer Kazmer <a@modules.io>
2021-09-03 13:39:25 -0400

    Update API

--- index.js
+++ index.js
@@ -1,72 +1,95 @@
 import * as THREE from 'three';
-import {GLTFLoader} from 'GLTFLoader';
-// import {BufferGeometryUtils} from 'BufferGeometryUtils';
-import {renderer, scene, app, physics, drop} from 'app';
 import easing from './easing.js';
+import metaversefile from 'metaversefile';
+const {useFrame, useActivate, useLoaders, usePhysics} = metaversefile;
 
-(async () => {
-  const u = app.files[`./chest.glb`];
-  let o = await new Promise((accept, reject) => {
-    new GLTFLoader().load(u, accept, function onprogress() {}, reject);
-  });
-  const {animations} = o;
-  o = o.scene;
-  app.object.add(o);
+const baseUrl = import.meta.url.replace(/(\/)[^\/\\]*$/, '$1');
 
-  o.updateMatrixWorld();
+export default () => {
+  const {gltfLoader} = useLoaders();
 
-  let baseMesh = null;
-  o.traverse(o => {
-    if (!baseMesh && o.isMesh && /base_container/i.test(o.name)) {
-      baseMesh = o;
-    }
-  });
-  const physicsId = physics.addGeometry(baseMesh);
+  const object = new THREE.Object3D();
   
-  const mixer = new THREE.AnimationMixer(o);
-  const actions = animations.map(animationClip => mixer.clipAction(animationClip));
-  /* let maxDuration = -Infinity;
-  for (const animation of animations) {
-    maxDuration = Math.max(maxDuration, animation.duration);
-  } */
-  const startOffset = 1;
-  const endOffset = 2;
-  const dropOffset = 1;
-  app.addEventListener('activate', e => {
-    // console.log('got activate');
-    
-    for (const action of actions) {
-      action.reset();
-      action.play();
-      action.time = startOffset;
-    }
-
-    let timeAcc = 0;
-    let lastUpdateTime = Date.now();
-    let dropped = false;
-    function animate() {
-      const now = Date.now();
-      const timeDiff = (now - lastUpdateTime) / 1000;
-      lastUpdateTime = now;
-      
-      timeAcc += timeDiff;
-      if (!dropped && timeAcc >= dropOffset) {
-        drop.drop(app.object, {
-          count: 10,
-        });
-        dropped = true;
-      }
-      if (timeAcc >= endOffset) {
-        // mixer.stopAllAction();
-        /* timeAcc = 0;
-        for (const action of actions) {
-          action.time = 0;
-        } */
-        app.removeEventListener('frame', animate);
-      } else {
-        mixer.update(timeDiff);
-      }
-    }
-    app.addEventListener('frame', animate);
+  let activateCb = null;
+  let frameCb = null;
+  useActivate(() => {
+    activateCb && activateCb();
   });
-})();
+  useFrame(() => {
+    frameCb && frameCb();
+  });
+
+  const physics = usePhysics();
+  (async () => {
+    const u = `${baseUrl}chest.glb`;
+    let o = await new Promise((accept, reject) => {
+      gltfLoader.load(u, accept, function onprogress() {}, reject);
+    });
+    const {animations} = o;
+    o = o.scene;
+    object.add(o);
+    
+    const dropObject = new THREE.Object3D();
+    dropObject.position.y = 0.5;
+    object.add(dropObject);
+
+    o.updateMatrixWorld();
+
+    let baseMesh = null;
+    o.traverse(o => {
+      if (!baseMesh && o.isMesh && /base_container/i.test(o.name)) {
+        baseMesh = o;
+      }
+    });
+    const physicsId = physics.addGeometry(baseMesh);
+    
+    const mixer = new THREE.AnimationMixer(o);
+    const actions = animations.map(animationClip => mixer.clipAction(animationClip));
+    /* let maxDuration = -Infinity;
+    for (const animation of animations) {
+      maxDuration = Math.max(maxDuration, animation.duration);
+    } */
+    const startOffset = 1;
+    const endOffset = 2;
+    const dropOffset = 1;
+    activateCb = () => {
+      // console.log('got activate');
+      
+      for (const action of actions) {
+        action.reset();
+        action.play();
+        action.time = startOffset;
+      }
+
+      let timeAcc = 0;
+      let lastUpdateTime = Date.now();
+      let dropped = false;
+      function animate() {
+        const now = Date.now();
+        const timeDiff = (now - lastUpdateTime) / 1000;
+        lastUpdateTime = now;
+        
+        timeAcc += timeDiff;
+        if (!dropped && timeAcc >= dropOffset) {
+          drop.drop(dropObject, {
+            count: 10,
+          });
+          dropped = true;
+        }
+        if (timeAcc >= endOffset) {
+          // mixer.stopAllAction();
+          /* timeAcc = 0;
+          for (const action of actions) {
+            action.time = 0;
+          } */
+          frameCb = null;
+        } else {
+          mixer.update(timeDiff);
+        }
+      }
+      frameCb = animate;
+    };
+  })();
+
+  return object;
+};
