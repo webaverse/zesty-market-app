4fec40d61e8a8e54b8baf47727572e10cef18e2b
Avaer Kazmer <a@modules.io>
2022-05-11 03:44:15 -0400

    Add InventoryBanner source

---
+++ InventoryBanner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './InventoryBanner.module.css';
+
+const InventoryBanner = () => {
+  return (
+    <div
+      className={styles.menu}
+      width={600}
+      height={400}
+  >
+      <h1>Ann's shop</h1>
+      <p>You break it you buy it!</p>
+  </div>
+  );
+};
+export default InventoryBanner;
